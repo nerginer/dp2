c941e0b3e910539a8c64df0e1ae0e7d9c354a09e
Nuri Erginer <nerginer@gmail.com>
2016-05-08 11:01:15 +0000

    frontpage design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,20 +8,38 @@
                 <div class="panel-heading"><h1>Makers Electronic Design Patterns</h1></div>
 
                 <div class="panel-body">
-                     <h4>
-                         
-                         <p>If you are willing to share your work and passion for electronics like us, here is the correct place to come together</p>
-                         <p>For turning great ideas into working products we have to look at what others did before.</p>
-                         <p> Building something new from a foundation of existing ideas is a key in creativity to make something better</p>
-                         <p>We try to bring the electronic building blocks together and call them <b>crowd-sourced library of Design Patterns</b></p>
-                         <p>Assemble Design Patterns like building blocks to quickly develop awesome new projects.</p>
-                     </h4>
-                      
+                     &nbsp;
+<h3><table style="width: 100%;">
+<tbody>
+<tr>
+<td style="text-align: center;"><img src="images/cometogether.jpg" alt="Mountain View" style="width:420px";></td>
+<td style="text-align: center;">If you are willing to share your work and passion for electronics like us, here is the correct place to come together</td>
+</tr>
+<tr>
+<td style="text-align: center;">For turning great ideas into working products we have to look at what others did before.</td>
+<td style="text-align: center;"><img src="images/greatproduct.jpg" alt="Mountain View" style="width:320px"></td>
+</tr>
+<tr>
+<td style="text-align: center;"><img src="images/key.png" alt="Mountain View" style="width:320px"></td>
+<td style="text-align: center;">Building something new from a foundation of existing ideas is a key in creativity to make something better</td>
+</tr>
+<tr>
+<td style="text-align: center;">We try to bring the electronic building blocks together and call them <b>crowd-sourced library of Design Patterns</b></td>
+<td style="text-align: center;"><img src="images/electronics.png" alt="Mountain View" style="width:320px"></td>
+</tr>
+<tr>
+<td style="text-align: center;"><img src="images/buildingblocks.jpg" alt="Mountain View" style="width:320px"></td>
+<td style="text-align: center;">Assemble Design Patterns like building blocks to quickly develop awesome new projects.</td>
+</tr>
+</tbody>
+</table></h3>
+&nbsp;<br><br><br>
+              
                       
                       
                     <h3>So what is a design Pattern?</h3>
                     <h4><p>A Design Pattern is a minimal functional block of electronic schematic which can be used as a module for more complex circuits.</p></h4>
-                     <b><h3><center> Join our growing community </center></h3></b>
+                    &nbsp;<br><br><br> <b><h3><center> Join our growing community </center></h3></b>
                 </div>
             </div>
         </div>
